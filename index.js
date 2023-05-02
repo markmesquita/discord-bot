3654eec2dd4873e4953b2e5ba52bc3e4468cab8e
Mark Mesquita <markmesquita@gmail.com>
2023-05-01 23:50:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+﻿// Require the necessary discord.js classes
+const dotenv = require(`dotenv`);
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+dotenv.config();
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+
+const commandsPath = path.join(__dirname, "commands");
+const commandsFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+for (const file of commandsFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`This command in ${filePath} has no "data" or "execute"`);
+  }
+}
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(TOKEN);
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error("Command not found");
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply("Error executing command");
+  }
+});
